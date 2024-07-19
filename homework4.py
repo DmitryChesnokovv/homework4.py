319345d2e1fe61fba3e69f4aa17a3b35e75a1891
DmitryChesnokovv <gawr03@yandex.ru>
2024-07-19 13:25:39 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,18 @@
+my_string = input("В какой школе вы учитесеь: ") # School Urban
+print(len(my_string))
+year_of_admission = 2024
+year_of_completion = input("Длительность обучения: ") # 1
+the_duration_of_the_training = year_of_admission + int(year_of_completion)
+print(the_duration_of_the_training)
+print(my_string.upper())
+print(my_string .lower())
+print(my_string .replace(" ", ""))
+first_char = my_string[0]
+print(first_char)
+last_char = my_string[11]
+print(last_char)
+
+
+
+
+
